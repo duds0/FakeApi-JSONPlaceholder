b62cb73b0ce9998541f04f0a2fdb5d849d39c127
duds0 <eduardofigueiredo1930@gmail.com>
2023-03-08 17:02:30 -0300

    Adiciona o texto digitado no final da página

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// https://jsonplaceholder.typicode.com/posts
+
 async function readPosts() {
     let postArea = document.querySelector(".posts");
     postArea.innerHTML = "Carregando";
@@ -9,33 +11,57 @@ async function readPosts() {
         postArea.innerHTML = "";
 
         for (let i in json) {
-            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`;
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`;4
             postArea.innerHTML += postHtml;
         }
     } else { postArea.innerHTML = "Nenhum post para exibir" }
 }
 
-function newPost() {
-    let title = document.querySelector("#titleField");
-    let body = document.querySelector("#bodyField");
-    let divP = document.querySelector(".posts");
+const startTime = performance.now();
+async function addNewPost(title, body) {
+    await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                body,
+                title,
+                userID: 7
+            })
+        }
+    ).then(response => {
+        const endTime = performance.now();
+        const responseTime = endTime - startTime;
+        console.log(`Tempo de resposta: ${responseTime}ms`);
+      });
 
-    let novoPost = `<div><h1>${title.value}</h1>${body.value}<hr/></div>`;
+    readPosts();
+};
 
-    divP.innerHTML += novoPost
+    document.querySelector("#insertButton").addEventListener("click", () => {
+        let title = document.querySelector("#titleField").value;
+        let body = document.querySelector("#bodyField").value;
 
-    title.value = "";
-    body.value = "";
-}
+        setTimeout(function myNewPost(){
+            let title = document.querySelector("#titleField");
+            let body = document.querySelector("#bodyField");
+            let divP = document.querySelector(".posts");
+        
+            let novoPost = `<div><h1>${title.value}</h1>${body.value}<hr/></div>`;
+        
+            divP.innerHTML += novoPost;
+        
+            title.value = "";
+            body.value = "";
+        }, 2000);
 
-document.querySelector("#insertButton").addEventListener("click", () => {
-    let title = document.querySelector("#titleField").value;
-    let body = document.querySelector("#bodyField").value;
+        if (title && body) {
+            addNewPost(title, body);
+        } else { alert("Preencha todos os campos!"); throw new Error("Preencha todos os campos") }
+    }
+    );
 
-    if (title && body) {
-        newPost();
-    } else { alert("Preencha todos os campos!"); throw new Error("Preencha todos os campos") }
-}
-)
-
-readPosts();
+    readPosts();

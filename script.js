68993ea13d483f2cfd9f4f221a2e3a91ff60288f
duds0 <eduardofigueiredo1930@gmail.com>
2023-04-13 11:29:34 -0300

    Formatação

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function readPosts() {
         postArea.innerHTML = "";
 
         for (let i in json) {
-            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`;4
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}<hr/></div>`; 4
             postArea.innerHTML += postHtml;
         }
     } else { postArea.innerHTML = "Nenhum post para exibir" }
@@ -36,32 +36,32 @@ async function addNewPost(title, body) {
         const endTime = performance.now();
         const responseTime = endTime - startTime;
         console.log(`Tempo de resposta: ${responseTime}ms`);
-      });
+    });
 
     readPosts();
 };
 
-    document.querySelector("#insertButton").addEventListener("click", () => {
-        let title = document.querySelector("#titleField").value;
-        let body = document.querySelector("#bodyField").value;
+document.querySelector("#insertButton").addEventListener("click", () => {
+    let title = document.querySelector("#titleField").value;
+    let body = document.querySelector("#bodyField").value;
 
-        setTimeout(function myNewPost(){
-            let title = document.querySelector("#titleField");
-            let body = document.querySelector("#bodyField");
-            let divP = document.querySelector(".posts");
-        
-            let novoPost = `<div><h1>${title.value}</h1>${body.value}<hr/></div>`;
-        
-            divP.innerHTML += novoPost;
-        
-            title.value = "";
-            body.value = "";
-        }, 2000);
+    setTimeout(function myNewPost() {
+        let title = document.querySelector("#titleField");
+        let body = document.querySelector("#bodyField");
+        let divP = document.querySelector(".posts");
 
-        if (title && body) {
-            addNewPost(title, body);
-        } else { alert("Preencha todos os campos!"); throw new Error("Preencha todos os campos") }
-    }
-    );
+        let novoPost = `<div><h1>${title.value}</h1>${body.value}<hr/></div>`;
 
-    readPosts();
+        divP.innerHTML += novoPost;
+
+        title.value = "";
+        body.value = "";
+    }, 2000);
+
+    if (title && body) {
+        addNewPost(title, body);
+    } else { alert("Preencha todos os campos!"); throw new Error("Preencha todos os campos") }
+}
+);
+
+readPosts();
